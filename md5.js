26b80fa689fa0526ff5b7f00c6e5df7e512fdd92
calmkart <494296669@qq.com>
2017-12-19 11:00:25 +0800

    Update md5.js
    改用ajax type head的方式替换GET的方式，用complete代替success，速度快了10倍以上

--- md5.js
+++ md5.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-    $("#b")  .click(function(){  
-        alert("11111111111");
-    });
+	var current_index = 1;
+	function get_md5(){
+		if(current_index==$("tr").length){
+			return;
+		}
+
+		var url = $("tr")[current_index].childNodes[0].childNodes[0].href;
+		$.ajax({
+        	url: url,           
+        	type: "HEAD",
+        	complete: function(jqXHR, textStatus) {
+        		var file_size = $("tr")[current_index].childNodes[1].childNodes[0].data;     
+        		var md5 = jqXHR.getResponseHeader("Content-MD5");
+        		console.log(jqXHR.getAllResponseHeaders());
+        		if(md5!=null && file_size!="-"){
+        			$("tr")[current_index].childNodes[1].innerHTML += ("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"+"[md5:"+md5+"]");
+        			}
+        		current_index++;
+        		get_md5();
+        		}
+    		});
+		}
+	get_md5();
 });
